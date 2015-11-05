c9fba3a8a1c441ea54f4d49343b28ee70ddf2302
Luke <lukem95@gmail.com>
2015-11-05 14:52:00 +0000

    Logo

---
+++ start.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+// First, print a console logo
+
+var logo = "" +
+"  __             _            _      _            _             " 			+ "\n" +
+" / _| ___  _ __ | |_       __| | ___| |_ ___  ___| |_ ___  _ __ " 			+ "\n" +
+"| |_ / _ \\| '_ \\| __|____ / _` |/ _ \\ __/ _ \\/ __| __/ _ \\| '__|" 	+ "\n" +
+"|  _| (_) | | | | ||_____| (_| |  __/ ||  __/ (__| || (_) | |   " 			+ "\n" +
+"|_|  \\___/|_| |_|\\__|     \\__,_|\\___|\\__\\___|\\___|\\__\\___/|_|   " + "\n" +
+"                                                                ";
+
+var subtitle = "+ - - - Identify fonts in the cloud! - - - - - - - - - - - - - +";
+
+console.log(logo);
+console.log(subtitle)
+console.log();
+
+// Now the serious work begins...
