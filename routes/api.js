bd85d263a466f2296dc6411179f870448fae75a5
lukem512 <lukem95@gmail.com>
2015-11-26 13:33:39 +0000

    Fixed path error

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ exports.upload = function (req, res) {
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
         console.log("Uploading: " + filename); 
-        fstream = fs.createWriteStream(__dirname + '../files/' + filename);
+        fstream = fs.createWriteStream(__dirname + '/../files/' + filename);
         file.pipe(fstream);
         fstream.on('close', function () {
             res.redirect('back');
